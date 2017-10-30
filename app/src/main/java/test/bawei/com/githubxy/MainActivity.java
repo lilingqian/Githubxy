b6d355c5ab3ed0610b3321978c078ee62023169e
lilingqian <1275021828@qq.com>
2017-10-30 19:51:09 +0800

    你说的代码

--- app/src/main/java/test/bawei/com/githubxy/MainActivity.java
+++ app/src/main/java/test/bawei/com/githubxy/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //提交第一
+        //李景钱的代码
+
     }
 }
